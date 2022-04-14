9894ed0f8156f569d6d9becf4192ce49ad35e043
shaoyi1997 <e0310084@u.nus.edu>
2022-04-14 20:49:03 +0800

    feat: add search page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@fontsource/montserrat-alternates/100.css";
+import "@fontsource/montserrat-alternates/200.css";
+import "@fontsource/montserrat-alternates/300.css";
+import "@fontsource/montserrat-alternates/400.css";
+import "@fontsource/montserrat-alternates/500.css";
+import "@fontsource/montserrat-alternates/600.css";
+import "@fontsource/montserrat-alternates/700.css";
+import "@fontsource/montserrat-alternates/800.css";
+import "@fontsource/montserrat-alternates/900.css";
+
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Montserrat Alternates, sans-serif",
+    body: "Montserrat Alternates, sans-serif",
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
